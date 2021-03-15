60fb5aedbe2dc1bdf6960efb9b1bfdbd48cab0ce
dj <dj@666.com>
2021-03-15 09:39:11 +0800

    dev提交

--- springmvc_final/ssm_xml/src/main/java/com/ex/ssm/entity/DevZZ.java
+++ springmvc_final/ssm_xml/src/main/java/com/ex/ssm/entity/DevZZ.java
@@ -1,4 +1,10 @@
 package com.ex.ssm.entity;
 
 public class DevZZ {
+    public void test(){
+        Account account = new Account();
+        Account account1 = new Account();
+        System.out.println(account);
+        System.out.println(account1);
+    }
 }
